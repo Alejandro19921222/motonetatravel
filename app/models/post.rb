df819cbf9efe76e810526d46577195140140fe8a
EC2 Default User <ec2-user@ip-172-31-18-27.ap-northeast-1.compute.internal>
2021-03-04 16:04:12 +0000

    finishCRUD

---
+++ app/models/post.rb
@@ -0,0 +1,7 @@
+class Post < ApplicationRecord
+  belongs_to :user
+  attachment :post_image
+
+  validates :title, presence: true
+	validates :body, presence: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_064347) do
+ActiveRecord::Schema.define(version: 2021_03_04_154244) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "post_image"
+    t.integer "draft"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +31,8 @@ ActiveRecord::Schema.define(version: 2021_03_03_064347) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.text "introduction"
+    t.string "profile_image_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
