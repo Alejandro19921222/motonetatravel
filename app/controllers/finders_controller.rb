237423cb6c23d493e4d92fe92ecef4cf8e9a4cc4
EC2 Default User <ec2-user@ip-172-31-18-27.ap-northeast-1.compute.internal>
2021-03-11 13:38:17 +0000

    finish seach

---
+++ app/controllers/finders_controller.rb
@@ -0,0 +1,12 @@
+class FindersController < ApplicationController
+  def finder
+    @range = params[:range]
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    elsif @range == "Post"
+      @posts = Post.looks(params[:search], params[:word])
+    else
+      @tags = Tag.looks(params[:search], params[:word])
+    end
+  end
+end
